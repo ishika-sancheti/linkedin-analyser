0261495beb17d4de0b427eb4045bbddc1e18dfc4
Ishika Sancheti <ishika.sancheti02@gmail.com>
2026-05-16 20:11:25 +0530

    Fixed the get_job_id and get _job_desc functions

--- scraper.py
+++ scraper.py
@@ -10,10 +10,20 @@ def get_job_id(inputTag):
     for element in inputTag:
         if re.match(r"urn:li:jobPosting:\d+", element['data-entity-urn']):
             listing=element['data-entity-urn']
-            job_id=listing.split(":")[-1]
-        job_id.append(job_id)
+            job_id_indv=listing.split(":")[-1]
+            job_id.append(job_id_indv)
     return job_id
 
+result = get_job_id(inputTag)
+print(result)
 
-# r2=requests.get("https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/4408775608")
-# print(r2.text)
+
+def get_job_desc(job_id):
+    r2=requests.get(f"https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{job_id}")
+    soup=BeautifulSoup(r2.text, 'html.parser')
+    job_desc=soup.find("div", class_="description__text")
+    job_desc_text=job_desc.get_text()
+    return job_desc_text
+    
+result2=get_job_desc("4408775608")
+print(result2)
