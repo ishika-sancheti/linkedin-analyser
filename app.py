7e8487418d3cb004b5beda8af6096fb381841f8c
Ishika Sancheti <ishika.sancheti02@gmail.com>
2026-05-18 09:45:23 +0530

    Added consolidation logic

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, jsonify
 from flask_cors import CORS
-
+from collections import Counter, defaultdict
+from scraper import scrape_all_jobs
 from analyzer import analyser_function
 import scraper
 
@@ -9,9 +10,16 @@ CORS(app)
 
 @app.route("/analyze", methods=['POST'])
 def analyser():
+    consolidated=defaultdict(Counter)
     smth=request.get_json()
-    res=analyser_function(smth["text"])
-    return jsonify(res)
+    keyword=smth["keyword"]
+    scraper_result=scrape_all_jobs(keyword)
+    for desc in scraper_result:
+        analyser_result=analyser_function(desc)
+        for category, skill in analyser_result.items():
+            consolidated[category].update(skill)
+    consolidated=dict(consolidated)
+    return jsonify(consolidated)
 
 if __name__=='__main__':
     app.run(debug=True, use_reloader=False)
